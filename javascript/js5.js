72ea36b33e3b1697db8c9ce64421acb41fe8a3dd
anoneon <36483414+anoneon@users.noreply.github.com>
2020-08-03 16:45:20 +0530

    Add files via upload

---
+++ javascript/js5.js
@@ -0,0 +1,12 @@
+console.log("hello from external js");
+
+function bigger(a,b){
+    if(a>b){
+        console.log("bigger : " + a);
+    }
+    else{
+        console.log("bigger : " + b);
+    }
+}
+
+bigger(2,45);
